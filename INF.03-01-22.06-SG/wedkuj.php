b7960d8baf3c2382ef2da955a847ef987528a055
Magic-Forces <root@foxlab.pl>
2024-11-17 18:54:12 +0100

    szkielet html

---
+++ INF.03-01-22.06-SG/wedkuj.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Wędkowanie</title>
+    <link rel="stylesheet" href="styl_1.css" />
+  </head>
+  <body>
+    <header><h1>Portal dla wędkarzy</h1></header>
+    <section id="l1">
+      <h3>Ryby zamieszkujące rzeki</h3>
+      <ol>
+        <!--TODO: Skrypt1-->
+      </ol>
+    </section>
+    <section id="p">
+      <img src="ryba1.jpg" alt="Sum" />
+      <br />
+      <a href="kwerendy.txt">Pobierz kwerendy</a>
+    </section>
+    <section id="l2">
+      <h3>Ryby drapieżne naszych wód</h3>
+      <table>
+        <tr>
+          <th>L.p.</th>
+          <th>Gatunek</th>
+          <th>Występowanie</th>
+        </tr>
+        <!--TODO: Skrypt2-->
+      </table>
+    </section>
+    <footer><p>Stronę wykonał: 0000000000</p></footer>
+  </body>
+</html>
